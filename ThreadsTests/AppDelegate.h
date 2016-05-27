74c46a9b8ed7e1d94fe689d32fa39574a97d9896
ShitikovAlexandr <shitikov.net@gmail.com>
2016-05-27 17:54:22 +0300

    Adding a repository

---
+++ ThreadsTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadsTests
+//
+//  Created by MacUser on 14.03.16.
+//  Copyright © 2016 Shitikov.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
